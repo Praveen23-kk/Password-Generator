d83425bd34411fb94c6db2d2c22b75520f6ee767
K Praveen Kumar <kkpraveen566@gmail.com>
2025-05-27 19:28:20 +0530

    second project

---
+++ passgen/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+def passgen(request):
+    char = list('abcdefghijklmnopqrstwxyz')
+    if request.GET.get('uppercase'):
+        char.extend(list('ABCDEFGHIJKLMNOPQRSTWUVZY'))
+    if request.GET.get('digits'):
+        char.extend(list('0123456789'))
+    if request.GET.get('symbol'):
+        char.extend(list('!@#$&'))  
+        
+    length = int(request.GET.get('length',10))
+              
+    password = ""
+    
+    for x in range(length):
+        password += random.choice(char)
+         
+    return render(request,'password.html',{'password':password})
